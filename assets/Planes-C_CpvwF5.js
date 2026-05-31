7da391283b5f3700eb20c47a51564a2a78a57e9b
AlejandroFigue <AlejandroFigue@users.noreply.github.com>
2026-05-31 20:44:26 +0000

    deploy: a770a379505ea9ed6943b636371b53c9dcc99594

---
+++ assets/Planes-C_CpvwF5.js
@@ -0,0 +1,1 @@
+import{u as a,j as n}from"./index-5gt-IOeK.js";import{P as o}from"./PlanAlimentario-Ba2C4JCl.js";function i(){const t=a(e=>e.pacienteId);return n.jsx(o,{pacienteId:t})}export{i as default};
