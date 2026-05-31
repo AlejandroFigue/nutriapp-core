0f25e9c3213c3307a1d9d49b18382c86b086f0aa
AlejandroFigue <AlejandroFigue@users.noreply.github.com>
2026-05-31 22:10:50 +0000

    deploy: 3ca776272632c081caa7e12560f599c34760c66d

--- assets/Planes-Bf1Ck3Az.js
+++
@@ -1,1 +0,0 @@
-import{u as a,j as n}from"./index-Bt4HH7WM.js";import{P as o}from"./PlanAlimentario-DiAfm34h.js";function i(){const t=a(e=>e.pacienteId);return n.jsx(o,{pacienteId:t})}export{i as default};
