d6a12b7242ac2979a7eaf31ecf8f0e993663674a
AlejandroFigue <AlejandroFigue@users.noreply.github.com>
2026-05-31 21:44:45 +0000

    deploy: 7627375b499ec22dd2a8d424358c7877b93ae454

--- assets/AnalysisDashboard-lqT8K03M.js
+++ assets/AnalysisDashboard-lqT8K03M.js
@@ -1,4 +1,4 @@
-import{r as h,u as $,j as a,l as I,d as j}from"./index-D2PXe-Gk.js";import{c as D,p as F}from"./costos-V0IoYBoe.js";const P=`
+import{r as h,u as $,j as a,l as I,d as j}from"./index-Bt4HH7WM.js";import{c as D,p as F}from"./costos-V0IoYBoe.js";const P=`
 /* ══ Wrapper ═══════════════════════════════════════════════════════════════ */
 .adash {
   max-width: 1060px;
